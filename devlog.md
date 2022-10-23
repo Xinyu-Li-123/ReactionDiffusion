f2241c7791566200725b4ba7bf4d05c685b35089
Eiger <xl3665@nyu.edu>
2022-10-23 01:08:48 -0400

    problem found: I neglect effects of external forces

--- devlog.md
+++ devlog.md
@@ -11,4 +11,14 @@ By negating the laplacian term (practically, negative diffusion coefficient), I 
 The real reason is that I write Laplacian matrix wrong. I write the negation of the Laplacian matrix...
 
 
-# Bug: Large initial concentration cause numerical instability
+# Bug: Large initial concentration / diffusion coefficient / Laplacian matrix cause numerical instability
+
+Large value of inital concentration / diffusion or Laplacian of large size will cause numerical instability.
+
+The numerical instability manifests in the form of negative concentration (which is physically impossible in our setting). 
+
+# Bug?: Seems like not diffusing enough...
+
+The gas seems to be too still. It only diffuses within a limited radius.
+
+This is because we ignore all forms of forces (gravity, levitation, etc)
--- simulation.py
+++ simulation.py
@@ -27,35 +27,37 @@ index2name = ["A", "B", "AB"]
 k1 = 0.5
 k2 = 0.5
 # diffusion coefficients of A, B, AB at x and y direction, a value in [0, 1]
-D_A = 1.
-D_B = 1.
-D_AB = 1.
+D_A = 17.
+D_B = 5.
+D_AB = 10.
 # feed / kill species A / B / AB at certain time
 #TODO: implement this feature
 
 # Define a 2d rectangle T. We will wrap it into a torus in the algorithm
 # T is a 3-dim array. Fix time t, T[0, x1, x2] is the concentration of species[0] 
 #   at location (x1, x2)
-T_size = (40, 40)
+T_size = (100, 100)
 T = np.zeros((len(species), *T_size), dtype=float)
-# T[0, :20, :20] = 100*np.random.random((20,20))
-# T[1, :20, :20] = 100*np.random.random((20,20))
-T[0,15:25,15:25] = 50*np.ones((10,10))
-T[1,5:15,5:15] = 70*np.ones((10,10))
-T[2,3:13,3:13] = 0*np.ones((10,10))
+T[0, :30, :30] = 50*np.random.random((30,30))
+T[1, 70:, 70:] = 50*np.random.random((30,30))
+
+# T[0,100:150,100:150] = 50*np.ones((50,50))
+# T[1,200:250,200:250] = 70*np.ones((50,50))
+# T[2,3:13,3:13] = 0*np.ones((10,10))
 
 
 ## Animation setting
 save_animation = False
 colormaps = ["Reds", "Blues", "Purples"]      # colormap for each species
-# cmap_min = -np.max(T)
+
+# cmap_min = -np.max(T)     # use this setting to visually check for negative concentration (numerical instability)
 cmap_min = np.min(T)
 cmap_max = np.max(T)
 
 
 ## Numerical solution setting
-dt = 0.05   # second
-interval = 10   # interval between frames, milisecond
+dt = 0.01   # second
+interval = 5   # interval between frames, milisecond
 playback_speed = dt * 1000 / interval
 total_frame = 5000
 print(f"dt = {dt}s, Playback speed: x{playback_speed:.1f}, Total time: {dt*total_frame:.2f}s")
@@ -65,11 +67,11 @@ Bstars = np.zeros(total_frame)
 Astars[0] = np.sum(T[[0,2],:,:])
 Bstars[0] = np.sum(T[[1,2],:,:])
 
-# laplacian_matrix = np.array([     # numerically unstable?
-#     [1, 1, 1],
-#     [1, -8, 1],
-#     [1, 1, 1],
-# ])
+laplacian_matrix = np.array([     
+    [1, 1, 1],
+    [1, -8, 1],
+    [1, 1, 1],
+])
 
 # laplacian_matrix = np.array([
 #     [0.05, 0.2, 0.05],
@@ -77,13 +79,44 @@ Bstars[0] = np.sum(T[[1,2],:,:])
 #     [0.05, 0.2, 0.05],
 # ])
 
-laplacian_matrix = 0.05 * np.array([     # larger laplacian
-    [1, 1, 1  , 1, 1],
-    [1, 1, 1  , 1, 1],
-    [1, 1, -24, 1, 1],
-    [1, 1, 1  , 1, 1],
-    [1, 1, 1  , 1, 1],
-])
+# laplacian_matrix = np.array([     # larger laplacian
+#     [1, 1, 1  , 1, 1],
+#     [1, 1, 1  , 1, 1],
+#     [1, 1, -24, 1, 1],
+#     [1, 1, 1  , 1, 1],
+#     [1, 1, 1  , 1, 1],
+# ])
+
+# In general, the Laplacian matrix of size N*N is 
+#   laplacian_matrix = np.ones((N, N))  
+#   laplacian_matrix[(N-1)/2, (N-1)/2] = -N*N+1
+# where N is an odd number
+
+# def gaussian_filter(kernel_size, sigma=1, muu=0):
+#     # Source: https://www.geeksforgeeks.org/how-to-generate-2-d-gaussian-array-using-numpy/
+#     # Initializing value of x,y as grid of kernel size
+#     # in the range of kernel size
+ 
+#     x, y = np.meshgrid(np.linspace(-2, 2, kernel_size),
+#                        np.linspace(-2, 2, kernel_size))
+#     dst = np.sqrt(x**2+y**2)
+ 
+#     # lower normal part of gaussian
+#     normal = 1/(2.0 * np.pi * sigma**2)
+ 
+#     # Calculating Gaussian filter
+#     gauss = np.exp(-((dst-muu)**2 / (2.0 * sigma**2))) * normal
+ 
+ 
+# kernel_size = 21
+# laplacian_matrix = gaussian_filter(kernel_size=kernel_size)
+
+
+
+N = 3
+laplacian_matrix = np.ones((N, N))    # an example of numerically unstable laplacian matrix
+laplacian_matrix[(N-1)//2, (N-1)//2] = -N*N+1
+
 
 def update(frame_index):
     global T
